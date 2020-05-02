efdbd0c5c9435e50d045abbe1c700802584df472
Maria Grazia Cabitza <mgcabitza@gmail.com>
2020-05-02 11:43:42 +0200

    comment added line 3, bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -1,5 +1,6 @@
 #file to insert in branch refactoring 
 #Use Python to understand U.S. bikeshare data. 
+#Calculate statistics and build an interactive environment where a user chooses the data and filter for a dataset to analyze.
 
 import time
 import pandas as pd
